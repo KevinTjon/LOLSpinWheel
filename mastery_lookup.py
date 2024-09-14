4c6c56d32d4baefe4e188876b69a61caf0122d0c
KevinTjon <kevintjon234@gmail.com>
2024-09-13 21:40:10 -0400

    Implement backend searching up league names and returning champion masteries

---
+++ mastery_lookup.py
@@ -0,0 +1,39 @@
+import requests
+from riot_api_key import DEV_API_KEY
+
+API_KEY = DEV_API_KEY # API key from riot_api_key.py
+REGION_1a = 'americas' # NOTE SOME GET REQUESTS USES 'americas' AND OTHERS USES 'na1' but both are still for NA region
+REGION_1b = 'na1'
+
+gameName = "Wagga" # TEST SUMMONER NAME
+tagLine = "bagga"
+
+# GET PUUID from summoner name, using LOL account name (eg. "Wagga#bagga")
+def get_summoner_puuid(gameName, tagLine):
+    url = f'https://{REGION_1a}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}'
+    headers = {'X-Riot-Token': API_KEY}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+count = 20 # Limits the number of top champions returned
+# GET TOP MASTERED CHAMPIONS limited to count
+def get_mastery_score(puuid):
+    url = f'https://{REGION_1b}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}/top'
+    headers = {'X-Riot-Token': API_KEY}
+    params = {'count': count}
+    response = requests.get(url, params=params, headers=headers)
+    
+    champion_ids = []
+    mastery_data = response.json()
+    for i in range(len(mastery_data)):
+        champion_id = mastery_data[i]['championId']
+        champion_ids.append(champion_id)
+    
+    return champion_ids
+
+response = get_summoner_puuid(gameName, tagLine)
+puuid = response['puuid']
+#print(puuid)
+mastery_score = get_mastery_score(puuid)
+print(mastery_score)
+
